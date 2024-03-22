1c2e153032d8f0a112d4a31d3599ad698713f005
Sidd Mittal <sidd12026@gmail.com>
2024-03-22 14:38:27 -0400

    cumulative csv + code updated

--- helper_functions.py
+++ helper_functions.py
@@ -3,7 +3,6 @@ import pandas as pd
 def drop_extraneous_col(df: pd.DataFrame) -> None:
     if "Unnamed: 0" in df.columns:
         df.drop("Unnamed: 0", axis=1, inplace=True)
-    
 
 def save_df(df: pd.DataFrame, name: str) -> None:
     """
@@ -18,4 +17,4 @@ def get_master_df() -> pd.DataFrame:
     """
     Get the most up-to-date master DF. Returns a DF.
     """
-    return pd.read_csv("csvs/master_df_updated.csv")
+    return pd.read_csv("csvs/master_df.csv")
