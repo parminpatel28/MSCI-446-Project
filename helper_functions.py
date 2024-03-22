ffe9d48e63e8ef9aa12116994c0da09264ba587d
Sidd Mittal <sidd12026@gmail.com>
2024-03-22 13:08:52 -0400

    added helper functions and updated master csv

---
+++ helper_functions.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+def save_df(df: pd.DataFrame, name: str) -> None:
+    """
+    Save a newly-created DF to the correct path. Returns None.
+    Params: 
+    - df: pd.DataFrame
+    - name: str
+    """
+    df.to_csv(f"csvs/{name}")
+
+def get_master_df() -> pd.DataFrame:
+    """
+    Get the most up-to-date master DF. Returns a DF.
+    """
+    return pd.read_csv("csvs/master_df_updated.csv")
